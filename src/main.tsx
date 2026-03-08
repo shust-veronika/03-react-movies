15df23a685c91edb7829c8ec1c332cabdcae6fd4
veronika <sustv153@gmail.com>
2026-03-08 18:31:42 +0200

    normalize-delet

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import "modern-normalize";
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
